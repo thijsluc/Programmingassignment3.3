84e60f619eadd0250c78fcef7d38f6988a2260ca
thijsluc <thijsluc@planet.nl>
2014-08-24 16:30:48 +0200

    Delivery 24/8/2014 Final

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
 Programmingassignment3.3
 ========================
 
-Repository for assignment week 3
+For making the tidy dataset I composed this set of different parts of available datasets. The different dataset I gave exact the same columnnames and combined
+the datasets (rowwise). Exact the same I did this for the datasets containing the contents for subject and activity.
+Input for making the final dataframe is the picture David Hood presented in the Discussion Forum in thread David's Course Projet FAQ. 
+
+In detail:
+
+Part 1:
+Read the data-files for train and test in a dataframe
+Read the headings-file (features) in a corresponding dataframe
+
+Make a charachter-vector headerx for storing the columnames of the features dataframe
+
+Use this vector as replacement for the columnnames in the data-frames train and test
+
+Join the train and test dataframe rowwise
+This is part1 dataframe
+
+Read the subject-train-data for train in a dataframe subject_train
+Read the subject-test-data for test in a dataframe subject_test
+Attach on both dataframes a columname Subject
+Join these two frames rowwise to get a new dataframe
+This is the part2 dataframe in the code
+
+Read the y_train and y_test txt-files in corresponding dataframes
+Replace the columnname by the name Activity
+Join these dataframes rowwise to one dataframe
+This is dataframe part3
+
+Read in the activity_labels txt-file in a dataframe activity_labels
+Give the columns new headers Activity and Description_activity
+
+To link dataframes part3 and activity_labels we use the join option
+between these two data-frames. Joining over Activity
+Only the description-column is selected by subsetting
+The output is dataframe part4
+
+Now I selected only those columns with the sentence std, mean, Subject or Activity in the columname.
+For this I made a select-list (selcols[]) with a vector which contained the required fields.
+
+The subsetting on the dataframes deliverd 4 dataframes with the wanted columns.
+With a columnbind (cbind) I combined these dataframes to one dataframe (tottmp)
+
+For part 3 of the course project I replaced the columnnames for Subject and Activity to a clean columnname.
+I did no extra work on the other columnnames (Part 4 of the course project)
+
+In step 5 I created the tidy-set by melting down the created set by Activity and Subject.
+
+With dcast I did a group by on Subject and Activity and calculated the mean of
+the group belonging to this group.
+
+At the end I write the dataframe to a txt-file
+
+
+
+
+
+the columname 
+
+
+
+
+
+
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,47 +1,109 @@
-## File : cachematrix.R
-## Date : 23/07/2014
+## File : run_analysis.R
+## Date : 23/08/2014
 ## Author: L. Thijssen
-## Comment: This script contains two functions which helps to compute the
-##          inverse of square matrix. As calculating the inverse matrix can
-##          be a timeconsuming job an extra function was designed to store the
-##          result of the last inversed matrix in the permanent-environment (memory).
-##          As the input matrix has not changed this environment can be read 
-##          to give the correct answer. 
-##          These scripts are based on Makevector and cachemean from
-##          the Coursera Course R Programming Assignment 2 Lexical scoping
+## Comment: 
 
-## makeCacheMatrix creates a special "matrix" object that can cache a inverse matrix
+## Start part 1 of assignment
+## Reading X_test.txt in a dataframe
+X_test <-read.table("X_test.txt")
+## Result dim(X_test) 2947 46
+X_train <-read.table("X_train.txt")
+## Result dim(X_train) 7352 561
+## Reading features.txt in a dataframe
+features <-read.table("features.txt")
+## dim 561 2
+## Only selecting the columnname from dataframe and putting in character vector
+headerx <-as.character(features$V2)
+## replacing columnheaders for X_train by headerx
+colnames(X_train) <- headerx
+## replacing columnheaders for X_test by headerx
+colnames(X_test)  <- headerx
+## Making first part of the final set (train and test combined with header)
+part1 <- rbind(X_train,X_test)
+## dim part 1 10299 561
 
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(z) m <<- z
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
-  
-}
+## Reading subject_train in dataframe subject_train
+subject_train <- read.table("subject_train.txt")
+## Reading subject_test in dataframe subject_test
+subject_test <- read.table("subject_test.txt")
+## Giving subject_test columname Subject 
+colnames(subject_test) <- c("Subject")
+## Idem subject_train
+colnames(subject_train) <- c("Subject")
+## Combining dataframs subject_train and subject_test to part2
+part2 <-  rbind(subject_train, subject_test)
+## dim part2 10299 1
+
+## Reading y_train in dataframe y_train
+y_train <- read.table("y_train.txt")
+## Reading y_test in dataframe y_test
+y_test <- read.table("y_test.txt")
+## Giving y_train columname Activity 
+colnames(y_train) <- c("Activity")
+## Idem y_test
+colnames(y_test) <- c("Activity")
+part3 <-  rbind(y_train, y_test)
+
+## Reading labels in dataframe 
+activity_labels <- read.table("activity_labels.txt")
+## Giving colnames to activity_labels
+colnames(activity_labels) <- c("Activity", "Description_Activity")
+
+## Join part3 and activity_labels into mergedData to get description in a data frame joining by Activity
+## load library plyr
+mergedData <- join(part3,activity_labels)
+## Make help dataframe 4 with contains only the Description_Activity
+part4 <- data.frame(mergedData[ ,c("Description_Activity")])
+
+## Now column-binding part1, part2 and part4 to the final dataframe final1
+final1 <- cbind(part1, part2, part4)
+## End of part 1 of assignment
 
 
-## cacheSolve computes the inverse of a matrix. If the inverse has already
-## been calculated (and the matrix has not changed) then the function should
-## retrieve the one from cache
+## Start part 2 of assignment
+#Make select-list for columns with std in columnname
+selcols1 <- grep("std", names(final1))
+#Make select-list for columns with mean in columnname
+selcols2 <- grep("mean", names(final1))
+#Make select-list for columns with Subject in columnname
+selcols3 <- grep("Subject",names(final1))
+#Make select-list for columns with Activity in columnname
+selcols4 <- grep("Activity", names(final1))
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached Inversed matrix")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data)
-  x$setinverse(m)
-  m
+## subsetting dataframes on columnselections
+stdtmp <-data.frame(final1[,selcols1])
+meantmp <-data.frame(final1[,selcols2])
+subtmp <-data.frame(final1[,selcols3])
+acttmp <-data.frame(final1[,selcols4])
 
-}
+## Combining seperate data.frames to one
+tottmp <-cbind(stdtmp,meantmp,subtmp, acttmp)
+## dim tottmp 10299 , 81
+## End of part 2 ofassignment
+
+## Start of part 3 giving correct activity names
+## Rename correct column to Subject cq Activity
+names(tottmp) [names(tottmp)== colnames(tottmp)[80]] <- "Subject"
+names(tottmp) [names(tottmp)== colnames(tottmp)[81]] <- "Activity"
+
+## End of part 3 of the assignment
+
+## Begin part 4 of assignment
+## End part 4 of assignment
+
+## Begin part 5 of assignment
+library(reshape2)
+## Melting dataframe to Subject and Activity
+tottmpmelt <- melt(tottmp, id = c("Subject","Activity"))
+## aggregatet tottmpmelt by Subject and Activity and get the mean of this group 
+tidyset <- dcast(tottmpmelt, Subject+Activity~ variable, mean)
+## Writing dataframe tidyset to txt-file
+write.table(tidyset,file="/tmp/tidyset.txt",row.names=F)
+## End part 5 of assignment
+
+
+
+
+
+
+
